f669c97e4b817e1848508c03ddcf42bd8c63cb9b
ObviousEcho <107782632+ObviousEcho@users.noreply.github.com>
2023-05-10 11:13:51 -0600

    Add files via upload

---
+++ Code/Fragments/src/components/Button.js
@@ -0,0 +1,12 @@
+const Button = () => {
+  return (
+    // Style attributes cannot be passed to fragments, which is why a div is being used here.
+    <div style={{ textAlign: "center" }}>
+      <button style={{ width: 200, height: 50, fontSize: 24 }}>
+        <strong>I &#x1F497; Fragments!</strong>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
